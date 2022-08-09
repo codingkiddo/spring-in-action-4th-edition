d20ddc83600c7c1175dc049b1b0863f2c542590d
Vinod Kumar <vinodkumar@Vinods-MacBook-Air.local>
2022-08-09 06:53:26 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# spring-mvc-java-demo
+# spring-mvc-java-demo
+# spring-in-action-4th-edition
---
+++ src/main/java/soundsystem/config/CDPlayerConfig.java
@@ -0,0 +1,18 @@
+package soundsystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import soundsystem.CDPlayer;
+import soundsystem.CompactDisc;
+
+@Configuration
+@Import(CDConfig.class)
+public class CDPlayerConfig {
+
+	@Bean
+	public CDPlayer cdPlayer(CompactDisc compactDisc) {
+		return new CDPlayer(compactDisc);
+	}
+}
